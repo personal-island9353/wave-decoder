2978656638885f9b66de3cb4f0af3ba503d4c2e3
personal.island9353 <personal.island9353@gmail.com>
2026-03-28 10:47:34 +0100

    [main] Parse and validate WAVE header

---
+++ src/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != 1 {
+		_, err := fmt.Fprintf(os.Stderr, "Usage: %s <file>\n", os.Args[0])
+		if err != nil {
+			os.Exit(1)
+			return
+		}
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(args[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(data) < 12 {
+		_, err := fmt.Fprintf(os.Stderr, "File %s is too small\n", os.Args[0])
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+
+	riff := binary.BigEndian.Uint32(data[:4])
+
+	if riff != 0x52494646 {
+		_, err := fmt.Fprintf(os.Stderr, "File %s contains invalid RIFF header\n", os.Args[0])
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+
+	chunkSize := binary.LittleEndian.Uint32(data[4:8])
+
+	format := binary.BigEndian.Uint32(data[8:12])
+	fmt.Printf("0x%08x\n", format)
+	if format != 57415645 {
+		_, err := fmt.Fprintf(os.Stderr, "File %s contains invalid WAVE format\n", os.Args[0])
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+
+	for i := uint32(12); i < chunkSize; i++ {
+		// TODO: parse file content
+	}
+}
