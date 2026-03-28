d3bcfb8fad20faeea1b0920cce71e33de87324c0
personal.island9353 <personal.island9353@gmail.com>
2026-03-28 11:44:28 +0100

    [main] Add playback support using oto library and handle 24-bit audio decoding

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/personal-island9353/wave-decoder/v2
 
 go 1.26.1
+
+require (
+	github.com/ebitengine/oto/v3 v3.4.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/ebitengine/oto/v3"
 )
 
 func main() {
@@ -42,10 +46,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	chunkSize := binary.LittleEndian.Uint32(data[4:8])
+	_ = binary.LittleEndian.Uint32(data[4:8]) // Read RIFF chunk size but don't strictly use it as we scan the whole slice
 
-	format := string(data[8:12])
-	if format != "WAVE" {
+	waveFormat := string(data[8:12])
+	if waveFormat != "WAVE" {
 		_, err := fmt.Fprintf(os.Stderr, "File %s contains invalid WAVE format\n", os.Args[0])
 		if err != nil {
 			os.Exit(2)
@@ -118,11 +122,89 @@ func main() {
 		os.Exit(1)
 	}
 
-	for i := uint32(36); i < chunkSize; i++ {
-		chunkId := string(data[i : i+4])
+	var audioData []byte
+	chunkOffset := 20 + fmtChunkSize
+
+	for chunkOffset+8 <= uint32(len(data)) {
+		chunkId := string(data[chunkOffset : chunkOffset+4])
+		chunkSize := binary.LittleEndian.Uint32(data[chunkOffset+4 : chunkOffset+8])
 
 		if chunkId == "data" {
-			// TODO: decode samples based on format
+			dataStart := chunkOffset + 8
+			dataEnd := dataStart + chunkSize
+			if dataEnd > uint32(len(data)) {
+				dataEnd = uint32(len(data))
+			}
+
+			if bitsPerSample == 24 {
+				for i := dataStart; i+3 <= dataEnd; i += 3 {
+					// Combine into a 24-bit signed integer (little-endian)
+					// data[i] is low byte, data[i+1] is mid byte, data[i+2] is high byte
+					b1 := uint32(data[i])
+					b2 := uint32(data[i+1])
+					b3 := uint32(data[i+2])
+
+					val24 := b1 | (b2 << 8) | (b3 << 16)
+
+					// Sign-extend from 24-bit to 32-bit
+					if val24&0x800000 != 0 {
+						val24 |= 0xFF000000
+					}
+
+					// Convert to 16-bit (signed)
+					// We take the top 16 bits of the 24-bit value
+					val16 := int16(int32(val24) >> 8)
+
+					// Append as little-endian 16-bit
+					audioData = append(audioData, byte(val16), byte(val16>>8))
+				}
+			} else {
+				audioData = append(audioData, data[dataStart:dataEnd]...)
+			}
+			break
 		}
+		chunkOffset += 8 + chunkSize
+	}
+
+	if len(audioData) == 0 {
+		_, err := fmt.Fprintln(os.Stderr, "Data chunks not found")
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+
+	var otoFormat oto.Format
+	switch bitsPerSample {
+	case 8:
+		otoFormat = oto.FormatUnsignedInt8
+	case 16, 24:
+		otoFormat = oto.FormatSignedInt16LE
+	default:
+		_, err := fmt.Fprintf(os.Stderr, "Unsupported bits per sample: %d\n", bitsPerSample)
+		if err != nil {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+
+	op := &oto.NewContextOptions{
+		SampleRate:   int(sampleRate),
+		ChannelCount: int(numChannels),
+		Format:       otoFormat,
+	}
+
+	otoCtx, readyChan, err := oto.NewContext(op)
+	if err != nil {
+		fmt.Printf("oto.NewContext failed: %v\n", err)
+		os.Exit(1)
+	}
+	<-readyChan
+
+	player := otoCtx.NewPlayer(bytes.NewReader(audioData))
+	player.Play()
+
+	for player.IsPlaying() {
+		time.Sleep(100 * time.Millisecond)
 	}
 }
